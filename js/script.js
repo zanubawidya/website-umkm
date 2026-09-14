14132f368f2f8f8570ad28cc3c56a0485cb43da2
zanubawidya <widyazanuba@gmail.com>
2026-09-14 13:11:51 +0700

    feat: finalisasi website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-    promoButtontextContent = "Promo: Beli 2 gratis tester!";
-    console.log("promo Kopi Nusa berhasil ditampilkan.");
+    promoButton.textContent = "Promo: Beli 2 gratis tester!";
+    console.log("Warkop Neng Widya berhasil ditampilkan.");
+
 });
